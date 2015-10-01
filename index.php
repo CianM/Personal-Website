d79e5427926857914e1ede170959190c526bf9d0
Cian Mackle <cianmackle@gmail.com>
2015-10-01 18:35:08 +0100

    Add images for About section

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 			  <a class="navbar-brand" href="index.php">Cian Mackle</a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			  <ul class="nav navbar-nav navbar-right">
+			  <ul class="nav navbar-nav navbar-right text-center">
 				<li><a href="#">About</a></li>
 				<li><a href="#">Portfolio</a></li>
 				<li><a href="#">Contact</a></li>
@@ -42,17 +42,17 @@
 						<img id="me" src="images/me.jpg" class="img-responsive center-block" alt="Me">
 						<p id="name">Cian<br>Mackle</p>
 						<div class="row">
-							<div class="col-md-4">
+							<div class="col-xs-4">
 								<a href="mailto:cian@cianmackle.com">
 									<img src="images/gmail.svg" class="img-responsive contact_images" alt="Email">
 								</a>
 							</div>
-							<div class="col-md-4">
+							<div class="col-xs-4">
 								<a href="#">
-									<img src="images/linkedin.svg" class="img-responsive contact_images" alt="LinkedIn">
+									<img src="images/linkedin.png" class="img-responsive contact_images" alt="LinkedIn">
 								</a>
 							</div>
-							<div class="col-md-4">
+							<div class="col-xs-4">
 								<a href="#">
 									<img src="images/github.svg" class="img-responsive contact_images" alt="Github">
 								</a>
@@ -74,11 +74,171 @@
 		</header>
 		
 		<!-- About ========== -->
-		<section>
+		<section class="container">
+			<div class="row text-center">
+				<h2>Skills</h2>
+			</div>
+			
+			<div class="row text-center">
+				<!-- CV / Resume ========== -->
+				<div class="col-xs-6">
+					<a href="#">
+						<img src="images/cv.svg" class="img-responsive" alt="Curriculum Vitae Link">
+						<label>Curriculum Vitae</label>
+					</a>
+				</div><!-- /cv -->
+				<div class="col-xs-6">
+					<a href="#">
+						<img src="images/cv.svg" class="img-responsive" alt="Resume Link">
+						<label>R&#233;sum&#233;</label>
+					</a>
+				</div><!-- /col -->
+			</div><!-- /row -->	
+			
+			<div class="row">
+				<div class="col-xs-12">
+					<p>
+						Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit​​, morbo vel maleficia?
+						De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium. 
+					</p>
+				</div><!-- /col -->
+			</div><!-- /row -->
+			
+			<!-- Strong skills ========== -->
+			<div class="row">
+				<div class="col-xs-12">
+					<h3 class="text-center">Strong</h3>
+				</div><!-- /col -->
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/html5.svg" alt="HTML5">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/css3.svg" alt="CSS3">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/javascript.svg" alt="Javascript">
+						</div>
+					</div>
+				</div><!-- /row (Thumbnails) -->
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/jquery.svg" alt="jQuery">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/git.svg" alt="Git">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/german.svg" alt="German">
+						</div>
+					</div>
+				</div><!-- /row (Thumbnails) -->
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/spanish.svg" alt="Spanish">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/strong/ukulele.svg" alt="Ukulele">
+						</div>
+					</div>
+				</div><!-- /row (Thumbnails) -->
+			</div><!-- /row (Strong skills) -->
+			
+			<!-- Skills that I'm improving on ========== -->
+			<div class="row">
+				<div class="col-xs-12">
+					<h3 class="text-center">Improving</h3>	
+				</div><!-- /col -->
+				
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/improving/angular.svg" alt="Angular.js">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/improving/sass.svg" alt="SASS">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/improving/android.svg" alt="Android">
+						</div>
+					</div>
+				</div><!-- /row (Thumbnails) -->
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/improving/gimp.png" alt="GIMP">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/improving/php.svg" alt="PHP">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/improving/mysql.svg" alt="MySQL">
+						</div>
+					</div>
+				</div><!-- /row (Thumbnails) -->
+			</div><!-- /row (Improving) -->
+			
+			<!-- Skills I would like to learn in the future ========== -->
+			<div class="row">
+				<div class="col-xs-12">
+					<h3 class="text-center">Future</h3>
+				</div><!-- /col -->
+				
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/future/rails.svg" alt="Rails">
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/future/ios.svg" alt="iOS">
+						</div>
+					</div>
+				</div><!-- /row (Thumbnails) -->
+			</div><!-- /row -->
+			
+			<!-- Side-Project Technologies -->
+			<div class="row">
+				<div class="col-xs-12">
+					<h3 class="text-center">Side Projects</h3>
+				</div><!-- /col -->
+				<div class="row">
+					<div class="col-xs-4">
+						<div class="thumbnail">
+							<img src="images/side_projects/raspberry_pi.svg" alt="Raspberry Pi">
+						</div>
+					</div>
+				</div><!-- /row (Thumbnails) -->
+			</div><!-- /row -->
 		</section><!-- /about -->
 		
 		<!-- Portfolio ========== -->
-		<section>
+		<section id="portfolio" class="container-fluid">
+			<div class="col-xs-12 text-center">
+				<h2>Portfolio</h2>
+			</div>
 		</section><!-- /portfolio -->
 		
 		<!-- Contact ======== -->
