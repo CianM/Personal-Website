cc467bef0758a557c352c45502d52a27ae8362ee
Cian Mackle <cianmackle@gmail.com>
2015-10-04 21:45:01 +0100

    Add navbar and header to portfolio pages. Update link to reference main page

--- portfolio/constructed_languages/index.php
+++ portfolio/constructed_languages/index.php
@@ -9,7 +9,59 @@
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/constructed_languages_style.css">
 	</head>
 	<body>
+	
+		<nav class="navbar navbar-default navbar-inverse">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a id="brand_name" class="navbar-brand lobster" href="index.php">Cian Mackle</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav navbar-right text-center open_sans">
+				<li><a href="../../index.php#about">About</a></li>
+				<li role="separator" class="nav-divider"></li>
+				<li><a href="../../index.php#skills">Skills</a></li>
+				<li role="separator" class="nav-divider"></li>
+				<li><a href="../../index.php#portfolio">Portfolio</a></li>
+				<li role="separator" class="nav-divider"></li>
+				<li><a href="../../index.php#contact">Contact</a></li>
+			  </ul>
+			</div><!-- /.navbar-collapse -->
+		  </div><!-- /.container -->		  
+		</nav>
+		
+		
+		
+		<footer class="container-fluid">
+			<nav class="row text-center">
+				<div class="col-xs-3">
+					<a href="../../index.php#about">About</a>
+				</div>
+				<div class="col-xs-3">
+					<a href="../../index.php#skills">Skills</a>
+				</div>
+				<div class="col-xs-3">
+					<a href="../../index.php#portfolio">Portfolio</a>
+				</div>
+				<div class="col-xs-3">
+					<a href="../../index.php#contact">Contact</a>
+				</div>
+			</nav><!-- /row -->
+		</footer><!-- /container -->
+	
+	
+		<!-- Scripts ========== -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		
+		
 	</body>
 </html>
