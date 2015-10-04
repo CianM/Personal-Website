4e5925cb96d9f2fec81605ad294bf92b45f130db
Cian Mackle <cianmackle@gmail.com>
2015-10-04 21:12:55 +0100

    Add index files and folders for Micro-Vision and Constructed Languages projects

---
+++ portfolio/constructed_languages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Portfolio | Constructed Languages</title>
+		<!-- <meta name="description" content="">
+		<meta name="keywords" content=""> -->
+		<link rel="icon" href="../../images/favicon.png" type="image/png">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+	</body>
+</html>
